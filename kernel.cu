b305903a6ae90fecfd8a8eb7f79d9b2dad80e3a2
Mike <38869540+ZeroMaster28@users.noreply.github.com>
2020-05-06 01:05:40 +0200

    fixed bugs added more comments

--- kernel.cu
+++ kernel.cu
@@ -13,34 +13,37 @@ long long int clock64();
 #include <stdio.h>
 #include <curand_kernel.h>
 
-// additional helping macros
+//Additional helper macros used along the code
 #define MAX_BLOCKS 65535
 #define HANDLE_ERROR(status) if(status != cudaSuccess) printf("\n Error...")
-
-//algorithm parameters i.e constants for scheme accuracy and parallel processing
+#define min(a,b) a<b?a:b
+//All algorithm parameters i.e constants for scheme accuracy and parallel processing.
 #define THREADS 100
 #define TRAJECTORIES 1000
 #define n 100
 #define N 1000
+#define cl_n 1000
 
-//parameters for SDE of the form 'dXt = a(t,Xt)*dt + b(t,Xt)dWt'
+//Parameters for SDE of the form 'dXt = a(t,Xt)*dt + b(t,Xt)dWt'.
 #define alpha 0.1209
 #define mu 0.0423
 #define sigma 0.1642 
 #define T 1.0
 #define x0 1.0
 
+//Stochastic drift 'a = a(t,x)'.
 __device__ double func_a(double t, double x)
 {
     return alpha*(mu - x);
 }
 
+//Stochastic diffusion 'b = b(t,x)'.
 __device__ double func_b(double t, double x)
 {
     return sigma*sqrt(x);
 }
 
-//returns squared difference value between exact and approximated solutions
+//Error metric function which returns squared difference value between exact and approximated solutions.
 __device__ double scheme_error(double* exact, double* non_exact)
 {
     double result = 0.0;
@@ -52,34 +55,64 @@ __device__ double scheme_error(double* exact, double* non_exact)
     return result;
 }
 
-//assigns vector of approximated realizations of XT to the solutions argument
-__global__ void classic_euler(double* solutions)
+//Functions assigns approximated realizations of random variable XT to the solutions argument.
+//Number of samples is based on the count parameter. Accuracy is easy to set by changing cl_n macro value.
+__global__ void classic_euler(double* solutions, int count)
 {
-    double X = x0;
     curandState_t state;
     curand_init(clock64() * blockDim.x, threadIdx.x, 0, &state);
-    int j;
-    double H = T / (n * N);
-    for (j = 0; j < N; j++)
-    {
-        X += func_a(j*H, X) * H + func_b(j * H, X) * curand_normal(&state) * sqrt(H);
+    
+    int trajectory_index = blockIdx.x;
+    while (trajectory_index < count) {
+        double X = x0;
+        int j;
+        double H = T / cl_n;
+        for (j = 0; j < cl_n; j++)
+        {
+            X += func_a(j * H, X) * H + func_b(j * H, X) * curand_normal(&state) * sqrt(H);
+        }
+        solutions[trajectory_index] = X;
+        trajectory_index += gridDim.x;
     }
-    solutions[blockIdx.x] = X;
 }
 
-//prints given number of samples of the approximated solutions which are computed due to parallel processing
-//additionally capable of saving it to the file if needed
+//This function is just a CPU member which performs the above kernel. Based on the arguments it might print
+//the results or save it to the file i.e for examining the probability distribution of XT etc. 
+//Samples are computed using at most half of the available blocks.
 void get_solution_samples(int samples, bool save_to_file=true, bool print=false)
 {
-    FILE* fptr = fopen("samples.txt", "w");;
+    
+    if (samples >= MAX_BLOCKS)
+    {
+        printf("TOO MANY SAMPLES! - ERROR");
+        return;
+    }
+    
+    FILE* fptr = fopen("samples.txt", "w");
     double* solutions = (double*) malloc(sizeof(double)*samples);
     double* solutions_dev;
+    
+    cudaEvent_t start, stop;
+    HANDLE_ERROR(cudaEventCreate(&start));
+    HANDLE_ERROR(cudaEventCreate(&stop));
+    HANDLE_ERROR(cudaEventRecord(start, 0));
     HANDLE_ERROR(cudaMalloc((void**)&solutions_dev, sizeof(double) * samples));
-    if (samples < MAX_BLOCKS) {
-        classic_euler<<<samples, 1 >>>(solutions_dev);
-        HANDLE_ERROR(cudaMemcpy(solutions, solutions_dev, sizeof(double*) * samples, cudaMemcpyDeviceToHost));
-    }
+
+    //In fact we choose to use at most half of the available blocks
+    classic_euler<<<min(samples, MAX_BLOCKS/2), 1 >>>(solutions_dev, samples);
+
+    HANDLE_ERROR(cudaMemcpy(solutions, solutions_dev, sizeof(double*) * samples, cudaMemcpyDeviceToHost));
     HANDLE_ERROR(cudaFree(solutions_dev));
+
+    HANDLE_ERROR(cudaEventRecord(stop, 0));
+    cudaEventSynchronize(stop);
+
+    float time;
+    HANDLE_ERROR(cudaEventElapsedTime(&time, start, stop));
+    printf("classic_euler kernel: %3.1f ms\n", time);
+    cudaEventDestroy(start);
+    cudaEventDestroy(stop);
+
     int i;
     for (i = 0; i < samples; i++) {
         if(print) printf("%f\n", solutions[i]);
@@ -89,25 +122,35 @@ void get_solution_samples(int samples, bool save_to_file=true, bool print=false)
     fclose(fptr);
 }
 
-// partial_mean- mean value of the solutions for every block, partial_error - mean error of the solutions per every block
+
+// Function is based on the reduction process used in parallel processing. For defined stochastic differential eqation
+// for which we do not know the exact solution it uses Euler-Maruyama scheme for finding the approximate solutions on
+// rare and dense grid. Then it calculates the average value of errors and the average value of samples obtained from XT.
+// Average values are calculated per block and saved to the given pointer arguments. Later on the final step consists of 
+// averaging of partial results.
 __global__ void euler_for_unknown(double* partial_mean, double* partial_error)
 {
-    __shared__ double error_for_thread[THREADS];
+    //Shared memory for some thread computed trajectory values.
+    //Notice that we suppose that kernel always runs with number 
+    //of threads which is not greater than THREADS 
+    __shared__ double error_for_thread[THREADS]; 
     __shared__ double solution_for_thread[THREADS];
 
+    //Every calculated trajectory can identified by single thread that performs computations
     int index = threadIdx.x + blockDim.x * blockIdx.x;
+    //Skipping if there might be more threads than actually needed
     if (index < TRAJECTORIES)
     {
         curandState_t state;
         curand_init(clock64()*blockIdx.x, threadIdx.x, 0, &state);
 
         double temp = x0, TEMP = x0;
-        double* x = (double*)malloc(sizeof(double) * n); // rozwi¹zania na rzadkiej
-        double* X = (double*)malloc(sizeof(double) * n); // rozwi¹zania na gêstej
+        double* x = (double*)malloc(sizeof(double) * n); // here goes rare grid solution
+        double* X = (double*)malloc(sizeof(double) * n); // and dense grid solution
 
         double curr_W = 0.0;
         double prev_W = 0.0;
-        double dW = 0.0;
+        double dW;
 
         double h = T / n;
         double H = h / N;
@@ -128,17 +171,21 @@ __global__ void euler_for_unknown(double* partial_mean, double* partial_error)
             X[i] = TEMP;
         }
 
+        //Assuming that dense grid solution is more accurate we save it
         solution_for_thread[threadIdx.x] = X[n - 1];
+        //Both dense and rare grid solutions are used to get the error value
         error_for_thread[threadIdx.x] = scheme_error(X, x);
         free(x);
         free(X);
     }
 
+    //Before doing the reduction it is important to synchronize all the threads within the block.
+    //Reason is to make sure that shared memory is fully initialized.
     __syncthreads();
 
     if (threadIdx.x == 0)
     {
-        //First thread is the one to manage reduction process
+        //First thread is chosen to be the one to manage reduction process
         double error_value = 0.0;
         double mean_value = 0.0;
         int i;
@@ -154,7 +201,8 @@ __global__ void euler_for_unknown(double* partial_mean, double* partial_error)
     }
 }
 
-//returns true if the device satisfies properties for parallel processing
+//Returns true if the device satisfies properties for parallel processing
+//i.e header defined macro values like used THREADS.
 bool is_enough_threads()
 {
     cudaDeviceProp prop;
@@ -165,7 +213,8 @@ bool is_enough_threads()
     return THREADS < max_threads_per_block;
 }
 
-// prints overall mean value of XT as well as approximation error
+//CPU member function which puprose is to finish the reduced problem of calculating mean value and error.
+//Additionally it prints the obtained results.
 void print_results(double* partial_mean, double* partial_error, int size)
 {
     int i;
@@ -175,12 +224,12 @@ void print_results(double* partial_mean, double* partial_error, int size)
         error += partial_error[i] * THREADS / TRAJECTORIES;
         mean += partial_mean[i] * THREADS / TRAJECTORIES;
     }
-    printf("error: %f\nmean: %f", error, mean);
+    printf("error: %f\nmean: %f\n", error, mean);
 }
 
 int main()
 {
-    //Validating if accuracy parameters aren't too demanding
+    //Validating if the accuracy parameters aren't too demanding
     if (!is_enough_threads()) {
         printf("Requesting for too many threads: %d", THREADS);
         return -1;
@@ -199,7 +248,7 @@ int main()
     double* partial_error = (double*)malloc(sizeof(double*)*BLOCKS);
     double* partial_error_dev;
 
-    //Events initialization for measuring time performance during kernel evaluation
+    //Events initialization for measuring time performance during kernels evaluations
     cudaEvent_t start, stop;
     HANDLE_ERROR(cudaEventCreate(&start));
     HANDLE_ERROR(cudaEventCreate(&stop));
@@ -209,7 +258,7 @@ int main()
     HANDLE_ERROR(cudaMalloc((void**)&partial_error_dev, BLOCKS * sizeof(double)));
     HANDLE_ERROR(cudaMalloc((void**)&partial_mean_dev, BLOCKS * sizeof(double)));
 
-    //calculating partial errors and means for the samples of XT
+    //Calculating partial errors and means for the samples of XT
     euler_for_unknown<<<BLOCKS,THREADS>>>(partial_mean_dev, partial_error_dev);
 
     HANDLE_ERROR(cudaMemcpy(partial_error, partial_error_dev, BLOCKS*sizeof(double), cudaMemcpyDeviceToHost));
@@ -219,11 +268,11 @@ int main()
     cudaFree(partial_mean_dev);
 
     HANDLE_ERROR(cudaEventRecord(stop, 0));
-
     cudaEventSynchronize(stop);
+
     float time;
     HANDLE_ERROR(cudaEventElapsedTime(&time, start, stop));
-    printf("GPU computations time: %3.1f ms\n", time);
+    printf("euler_for_unknown kernel: %3.1f ms\n", time);
     cudaEventDestroy(start);
     cudaEventDestroy(stop);
 
@@ -233,8 +282,8 @@ int main()
     free(partial_mean);
     free(partial_error);
 
-    //printing and additionally saving given number of samples from XT
-    //useful for exploring the probability distribution
+    //Printing and additionally saving given number of samples from random variable XT
+    //This is useful for example for exploring the probability distribution of XT
     get_solution_samples(10000);
 
     return 0;
